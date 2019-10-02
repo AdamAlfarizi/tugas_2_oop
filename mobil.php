e5f797ec5710227873a89426ecc07154be8e1e80
adam <adamalfal8@mail.unpas.ac.id>
2019-10-02 18:50:11 +0700

    membuat properti dan method pada sebuah kelas produk.php dan membuat file mobil.php

---
+++ mobil.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+ class Mobil {
+ 	public $nama,
+ 	public $merk,
+ 	public $warna,
+ 	public $kecepatanMaksimal,
+ 	public $jumlahPenumpang;
+
+ 	public function_exists tambahKecepatan(){
+
+ 	}
+ 	public function_exists kurangiKecepatan(){
+
+ 	}
+ 	public function_exists gantiTraspormasi(){
+ 		
+ 	}
+ } 

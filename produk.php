18bfc4bf42a8106e94f95089f00c3fc400a93af2
adam <adamalfal8@mail.unpas.ac.id>
2019-10-03 02:22:01 +0700

    menambah inheritance membaut proprti baru jmlHalaman,waktuMain dan tipe menambahkan parameter baru membuat method baru getInfoLengkap di file produk.php

--- produk.php
+++ produk.php
@@ -9,18 +9,36 @@ class Produk{
 	public  $judul,
 			$penulis,
 			$penerbit,
-			$harga ;
+			$harga ,
+			$jmlHalaman,
+			$waktuMain,
+			$tipe;
 
-			public function __construct($judul= "judul",$penulis= "penulis",$penerbit = "penerbit",$harga= 0){
+			public function __construct($judul= "judul",$penulis= "penulis",$penerbit = "penerbit",$harga= 0,$jmlHalaman = 0,$waktuMain = 0,$tipe){
 				$this->judul = $judul;
 				$this->penulis = $penulis;
 				$this->penerbit = $penerbit;
 				$this->harga = $harga;
+				$this->jmlHalaman = $jmlHalaman;
+				$this->waktuMain = $waktuMain;
+				$this->tipe = $tipe;
+
 
 			}
 
 	public function getLabel(){
 		return "$this->penulis, $this->penerbit";
+	}
+
+	public function getInfoLengkap(){
+		$str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+		if ($this->tipe == "Komik") {
+			$str .= "-{$this->jmlHalaman} Halaman.";
+		}else if ($this->tipe == "Game") {
+			$str .= "- {$this->waktuMain} jam.";
+		}
+
+		return$str;
 	}
 
 }
@@ -33,17 +51,10 @@ class CetakInfoProduk{
 	}
 }
 
-$produk1 = new Produk("Naruto","Masashi Kishimoto","Shonen Jump",30000);
+$produk1 = new Produk("Naruto","Masashi Kishimoto","Shonen Jump",30000,100, 0,"Komik");
+$produk2 = new Produk("uncharted","Neil Druckman","Sony Computer",250000,0,50,"Game");
 
 
-$produk2 = new Produk("uncharted","Neil Druckman","Sony Computer",250000);
-
-
-
-echo "Komik : " .$produk1->getLabel();
+echo $produk1->getInfoLengkap();
 echo "<br>";
-echo "Game :  " .$produk2->getLabel();
-
-echo "<br>";
-$infoProduk1 = new CetakInfoProduk();
-echo $infoProduk1->cetak($produk1);
+echo $produk2->getInfoLengkap();

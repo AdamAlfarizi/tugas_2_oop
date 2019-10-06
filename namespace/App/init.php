997a371c342adb7534c0f64d3242dc253ed5dc50
adam <adamalfal8@mail.unpas.ac.id>
2019-10-07 05:07:53 +0700

    mebuat Namespace

--- namespace/App/init.php
+++ namespace/App/init.php
@@ -5,6 +5,17 @@
 // require_once'Produk/Game.php';
 // require_once'Produk/CetakInfoProduk.php';
 
+// require_once'Produk/User.php';
+
+// require_once'Service/User.php';
+
 spl_autoload_register(function($class){
+	$class = explode('\\' , $class);
+	$class = end($class);
 	require_once 'Produk/' . $class . '.php';
+});
+spl_autoload_register(function($class){
+	$class = explode('\\' , $class);
+	$class = end($class);
+	require_once 'Service/' . $class . '.php';
 });
